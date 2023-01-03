05610a374ebdc573dbc06f41eb00a87ac482ab4e
Samuel King <SKingSolutions@gmail.com>
2023-01-03 13:50:28 +0000

    Added logic to work out which coins are used and prints them. Need to make it more consise and say how many times each coin is used (e.g rather than 20p 20p 20p I want it to say 3 20ps)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        List<Integer> coins = Arrays.asList(200,100,50,20,10,5,1); //List of coins
+        int sum = 27;                                             //Amount of money
+
+        while(sum > 0){
+            for(Integer i: coins){                                //checks through list of coins
+                if(sum >= i){
+                    sum = sum - i;                                //subtracts it from the sum if it can
+                    if(i <= 50){
+                        System.out.println(i+ "p");
+                    }else if(i == 100){
+                        System.out.println("£1");
+                    }else{System.out.println("£2");}
+                    // optional section for putting money into pounds and pence  rather than integer pence
+                    break;
+                }
+            }
+        }
+
+    }
 }
